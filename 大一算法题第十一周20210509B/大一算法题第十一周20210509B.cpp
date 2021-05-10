fbfd12802d9bf029061cd2b0716d159e097f0cd4
huicong2002 <1532751400@qq.com>
2021-05-10 23:12:48 +0800

    logs

---
+++ 大一算法题第十一周20210509B/大一算法题第十一周20210509B.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	long long int arr[55];
+	arr[0] = 0;
+	arr[1] = 1;
+	arr[2] = 2;
+	for (int i = 3; i < 55; i++)
+	{
+		arr[i] = arr[i - 1] + arr[i - 2];
+	}
+
+	int x;
+	scanf("%d", &x);
+	long long int X = arr[x];
+
+	printf("%lld", X);
+	
+	return 0;
+}
